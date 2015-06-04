494bfef0483c263d4688de2ac2d8fb0066cbf0eb
CoderJ <coderj@foxmail.com>
2015-06-04 10:05:10 +0800

    add title

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var data = require('../data/data');
 var http = require('http');
 var iconv = require('iconv-lite');
+var cheerio = require('cheerio');
 /* GET home page. */
 router.get('/list/', function(req, res) {
   res.set('Content-Type', 'application/json');
@@ -40,9 +41,46 @@ router.get('/money/', function(req, res) {
     });
 
     req.end();
+});
+
+router.get('/income/', function(req, res) {
+    res.set('Content-Type', 'application/json');
+    var num = req.query.num || '000729';
+    var url = '/jxjj/qxcp/jzcxjsp.jsp?code='+num;
 
 
+    var options = {
+        hostname: 'www.ccbfund.cn',
+        port: 80,
+        path: url,
+        method: 'GET'
+    };
 
+    var req = http.request(options, function (rs) {
+        rs.setEncoding('utf8');
+        rs.on('data', function (chunk) {
+            var $ = cheerio.load(chunk);
+            $("table tr").each(function(){
+                var data = {};
+                var td = $(this).find("td");
+                if(td.length<1){
+                    return ;
+                }else{
+                    var data = {
+                        date : td[0].children[0].data.replace(/-/g,'/'),
+                        price : parseFloat(td[1].children[0].data)
+                    };
+                }
+            });
+            res.send(200);
+        });
+    });
+
+    req.on('error', function (e) {
+        res.send('problem with request: ' + e.message);
+    });
+
+    req.end();
 });
 
 module.exports = router;
